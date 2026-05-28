f2c88d55f0d29789d542819407c6579dac442029
Yevhen Skrypka <cash4dog@gmail.com>
2026-05-29 02:41:19 +0300

    test change response in app

--- back_server.py
+++ back_server.py
@@ -6,15 +6,20 @@ class SimpleHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/plain; charset=utf-8')
             self.end_headers()
-            self.wfile.write(b"Hello, Lera!")
+
+            pod_ip = socket.gethostbyname(socket.gethostname())
+                        
+            response_message = f'{{"status": "UP", "pod_ip": "{pod_ip}"}}'
+            self.wfile.write(response_message.encode('utf-8'))
         else:
             self.send_response(404)
             self.end_headers()
 
 def run():
-    server_address = ('0.0.0.0', 9900)
+    port = 9900
+    server_address = ('0.0.0.0', port)
     httpd = HTTPServer(server_address, SimpleHandler)
-    print("Сервер запущено на порту 9900...")
+    print(f"Сервер запущено на порту {port}. Очікування запитів...")
     httpd.serve_forever()
 
 if __name__ == '__main__':
